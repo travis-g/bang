ee5c0b264df94dbc9517b6067519eb918b1b328c
Travis Grammer <travisjgrammer@gmail.com>
2019-08-17 19:16:04 -0400

    fix go-lint complaint

--- main.go
+++ main.go
@@ -16,11 +16,11 @@ func loadConfig() {
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
 	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		panic(fmt.Errorf("fatal error config file: %s \n", err))
 	}
 	err = viper.Unmarshal(&Bangs)
 	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		panic(fmt.Errorf("fatal error config file: %s \n", err))
 	}
 }
 
